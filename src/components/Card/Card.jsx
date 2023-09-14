9f8f82505022ec2f97040df1694019a1d844556b
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-14 15:57:59 -0300

    agrego props y componentes dinamicos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,24 @@
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
+  const { titulo, imagen } = props;
   // logica
 
   return (
     //jsx
     <article className='card'>
       <div className='card-header'>
-        <img src="https://media.revistagq.com/photos/607d3e0c4286e6a59febbacc/master/pass/los-simpson.jpg" alt="Imagen de los simpsons" />
+        <img src={imagen} alt='Imagen de los simpsons' />
       </div>
       <div className='card-body'>
-        <h2>Los simpsons</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit quidem magni fuga, at ut alias?</p>
+        <h2>{titulo}</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+          quidem magni fuga, at ut alias?
+        </p>
       </div>
     </article>
   );
-}
+};
 
 export default Card;
--- src/components/CardsGallery/CardsGallery.jsx
+++ src/components/CardsGallery/CardsGallery.jsx
@@ -2,12 +2,33 @@ import Card from '../Card/Card';
 
 import './CardsGallery.css';
 
+const info = [
+  {
+    id: 1,
+    titulo: 'los increibles',
+    imagen:
+      'https://fotografias.larazon.es/clipping/cmsimages01/2019/08/06/EAABE530-50DF-4836-A39E-1076938BC604/98.jpg?crop=962,541,x56,y0&width=1900&height=1069&optimize=low&format=webply',
+  },
+  {
+    id: 2,
+    titulo: 'Los simpsons',
+    imagen:
+      'https://media.revistagq.com/photos/607d3e0c4286e6a59febbacc/master/pass/los-simpson.jpg',
+  },
+  {
+    id: 3,
+    titulo: 'Mario',
+    imagen:
+      'https://elcomercio.pe/resizer/ogmrm_zqIO6bNsdb3sjIyao1Un0=/1200x800/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/3HHLTK3N3NDSVGVDJTL7H7Y6KQ.jpg',
+  },
+];
+
 const CardsGallery = () => {
   return (
     <section className='cards-container'>
-      <Card />
-      <Card />
-      <Card />
+      {info.map((item) => {
+        return <Card key={item.id} titulo={item.titulo} imagen={item.imagen} />;
+      })}
     </section>
   );
 };
