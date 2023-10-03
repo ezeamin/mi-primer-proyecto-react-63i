5f53528733e96b81a5092cbe017ab1b9b4688f38
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-03 14:58:00 -0300

    agrego local storage

--- src/components/Contactos/Contactos.jsx
+++ src/components/Contactos/Contactos.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Formv3 from '../Form/Formv3';
 import List from '../Form/List';
 
+const contactosLS = JSON.parse(localStorage.getItem('contactos')) || [];
+
 const Contactos = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(contactosLS);
+
+  useEffect(() => {
+    localStorage.setItem('contactos', JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <>
-      <Formv3 setContacts={setContacts}/>
-      <List contacts={contacts}/>
+      <Formv3 setContacts={setContacts} />
+      <List contacts={contacts} />
     </>
   );
 };
