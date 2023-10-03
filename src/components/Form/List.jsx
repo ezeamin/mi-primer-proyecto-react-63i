ad1c7ea8d93cd6fc652101bb27bc1ef62e676b28
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-03 15:02:28 -0300

    agrego ejemplo de renderizado condicional

--- src/components/Form/List.jsx
+++ src/components/Form/List.jsx
@@ -5,6 +5,7 @@ const List = (props) => {
 
   return (
     <ul>
+      {contacts.length === 0 && <p>No hay contactos</p>}
       {contacts.map((contact) => (
         <Item key={contact.id} contact={contact} />
       ))}
